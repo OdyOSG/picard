9e49b4a145a9e394c6aa9f69135f68e6d3fc60e4
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-01-20 17:24:55 -0500

    rearrange template structure

--- R/ohdsi_project.R
+++ R/ohdsi_project.R
@@ -4,11 +4,24 @@
 #'
 #' @export
 ohdsi_project <- function(path) {
-  path <- fs::path_expand_r(path) %>%
-    fs::dir_create()
+  path <- fs::path_expand_r(path)
+  # create project directory
+  fs::dir_create(path)
+
   template_dir <- fs::path_package("picard", "templates/ohdsi_study")
   r <- move_files(from_path = template_dir,
                   to_path = path)
+  #create input directory
+  fs::dir_create(fs::path(path, "input"))
+  #create output directory
+  fs::dir_create(fs::path(path, "output"))
+  #create R directory
+  fs::dir_create(fs::path(path, "R"))
+
+  #create R directory
+  fs::dir_create(fs::path(path, "extras"))
+  fs::file_move(path = fs::path(path, "Welcome.Rmd"),
+                new_path = fs::path(path, "extras/Welcome.Rmd"))
 
   invisible(r)
 }
