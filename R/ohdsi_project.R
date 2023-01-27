c54e176c066133f29c5eef760727dd444ab877dc
Nadia Kadakova <nadzeya.kadakova@gmail.com>
2023-01-27 14:49:36 +0200

    upd folder structure + Welcome.Rmd

--- R/ohdsi_project.R
+++ R/ohdsi_project.R
@@ -7,9 +7,11 @@ ohdsi_project <- function(path) {
   path <- fs::path_expand_r(path) %>%
     fs::dir_create()
   template_dir <- fs::path_package("picard", "templates/ohdsi_study")
-  r <- move_files(from_path = template_dir,
-                  to_path = path)
-
+  from <- template_dir
+  to <- path
+  r <- file.copy(list.files(from, full.names = TRUE),
+                             to,
+                             recursive = TRUE)
   invisible(r)
 }
 
