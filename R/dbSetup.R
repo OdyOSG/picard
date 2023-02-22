8b007299fc23bb5cb28d084f1e4ce658193cb848
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-02-21 19:34:31 -0500

    remove schema functions

--- R/dbSetup.R
+++ R/dbSetup.R
@@ -2,6 +2,8 @@
 #' @param configBlock the configuration block in the config.yml file
 #' @param type choose type of tables to initialize either analysis or diagnostics
 #' set as the active configuration
+#' @param configFile the config.yml file to add a configBlock. Default to config.yml in project
+#' directory
 #' @export
 initializeCohortTables <- function(configBlock,
                                    type = c("analysis", "diagnostics"),
@@ -34,6 +36,7 @@ initializeCohortTables <- function(configBlock,
                                       cohortDatabaseSchema = write_schema,
                                       cohortTableNames = cohortTableNames,
                                       incremental = TRUE)
+  invisible(cohortTableNames)
 
 }
 
@@ -58,6 +61,8 @@ drop_tbl <- function(connectionDetails,
 #' @param configBlock the configuration block in the config.yml file
 #' @param type choose type of tables to initialize either analysis or diagnostics
 #' set as the active configuration
+#' @param configFile the config.yml file to add a configBlock. Default to config.yml in project
+#' directory
 #' @export
 dropCohortTables <- function(configBlock, type = c("analysis", "diagnostics"),
                              configFile = here::here("config.yml")) {
@@ -90,58 +95,14 @@ dropCohortTables <- function(configBlock, type = c("analysis", "diagnostics"),
                                           table_name =  .x))
 }
 
-
-#' Create Write Schema
-#' @param configBlock the configuration block in the config.yml file
-#' @export
-createWriteSchema <- function(configBlock,
-                              configFile = here::here("config.yml")) {
-
-  connectionDetails <- config::get("connectionDetails",
-                                   config = configBlock,
-                                   file = configFile)
-  write_schema <- config::get("write",
-                              config = configBlock,
-                              file = configFile)
-
-  #set up database connection
-  conn <- DatabaseConnector::connect(connectionDetails = connectionDetails)
-  on.exit(DatabaseConnector::disconnect(conn))
-
-  sql <- "CREATE SCHEMA IF NOT EXISTS @write_schema AUTHORIZATION @user;" %>%
-    SqlRender::render(write_schema = write_schema,
-                      user = connectionDetails$user()) %>%
-    SqlRender::translate(targetDialect = connectionDetails$dbms,
-                         tempEmulationSchema = NULL)
-  DatabaseConnector::executeSql(conn, sql)
-  invisible(configBlock)
-
-}
-
-
-
-#' Drop Write Schema
-#' @param configBlock the configuration block in the config.yml file
-#' @export
-dropWriteSchema <- function(configBlock,
-                            configFile = here::here("config.yml")) {
-
-  connectionDetails <- config::get("connectionDetails",
-                                   config = configBlock,
-                                   file = configFile)
-  write_schema <- config::get("write",
-                              config = configBlock,
-                              file = configFile)
-
-  #set up database connection
-  conn <- DatabaseConnector::connect(connectionDetails = connectionDetails)
-  on.exit(DatabaseConnector::disconnect(conn))
-
-  sql <- "DROP SCHEMA IF EXISTS @write_schema CASCASE;" %>%
-    SqlRender::render(write_schema = write_schema) %>%
-    SqlRender::translate(targetDialect = connectionDetails$dbms,
-                         tempEmulationSchema = NULL)
-  DatabaseConnector::executeSql(conn, sql)
-  invisible(configBlock)
-
+initialize_cohort_tables <- function(projectSpecs) {
+  cli::cat_bullet("Step 6: Initialize cohort tables",
+                  bullet_col = "green", bullet = "info")
+  configFile <- fs::path(projectSpecs$location, projectSpecs$projectName, "config.yml")
+  cb <- listConfigBlocks(configFile)
+  purrr::walk(cb, ~initializeCohortTables(.x, type = "analysis", configFile = configFile))
+  if (projectSpecs$addDiagnostics) {
+    purrr::walk(cb, ~initializeCohortTables(.x, type = "diagnostics", configFile = configFile))
+  }
+  invisible(cb)
 }
