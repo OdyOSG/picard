dc5bc75a13b564bb15dd42eac0fe316f0c7e29f5
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-02-23 08:43:44 -0500

    add pipeline task function

--- R/picardScripts.R
+++ R/picardScripts.R
@@ -33,11 +33,14 @@ buildDependcies <- function(dependencies) {
 #' @param scriptName the name of your project script
 #' @param ... a list of additional inputs used to annotate your script. Names of inputs
 #' must appear in snakecase. For example study_contact = "John Smith"
+#' @param dependencies the list of dependencies for the r script
+#' @param openFile a T/F toggle specifying whether to open file in Rstudio
 #' @param path the path to write the R file, default is the R folder of the active project
 #' @export
 newRScript <- function(scriptName,
                        ...,
                        dependencies = c(),
+                       openFile = TRUE,
                        path = here::here("R")) {
 
 
@@ -50,9 +53,88 @@ newRScript <- function(scriptName,
   filePath <- fs::path(path, fileName)
 
   readr::write_lines(txt, file = filePath)
-  cli::cat_bullet(crayon::cyan(fileName), " generated at ", path,
+  cli::cat_bullet(crayon::green(fileName), " generated at ", crayon::cyan(path),
                   bullet = "tick", bullet_col = "green")
   #open file
-  rstudioapi::navigateToFile("~/R/tests/my_ohdsi_project/R/BuildCohorts.R")
+  if (openFile) {
+    rstudioapi::navigateToFile(filePath)
+  }
+
   invisible(txt)
+}
+
+
+#' Create a new pipeline task
+#' @description This function creates a R script and sourced script for functions and a
+#' folder in the output folder. The r script is labeled with the step in the pipeline
+#' @param scriptName the name of your project script
+#' @param taskNumber the task number in the pipeline
+#' @param ... a list of additional inputs used to annotate your script. Names of inputs
+#' must appear in snakecase. For example study_contact = "John Smith"
+#' @param dependencies the list of dependencies for the r script
+#' @param openFile a T/F toggle specifying whether to open file in Rstudio
+#' @param path the path to write the R file, default is the active project
+#' @export
+newPipelineTask <- function(scriptName,
+                            taskNumber,
+                            ...,
+                            dependencies = c(),
+                            openFile = TRUE,
+                            path = here::here()) {
+
+  #label
+  taskNumber <- scales::label_number(prefix = "0")(taskNumber)
+  pipelineNm <- paste(taskNumber, paste0(scriptName, ".R"), sep = "_")
+
+  #create internals file
+  sourceScriptFile <- paste0("_", scriptName, ".R")
+  r_int_dir <- fs::path(path, "R/internals")
+  if (!fs::dir_exists(r_int_dir)) {
+    fs::dir_create(r_int_dir)
+    cli::cat_bullet("Created Directory: ", crayon::cyan(r_int_dir),
+                    bullet = "tick",
+                    bullet_col = "green")
+  }
+  fs::path(r_int_dir, sourceScriptFile) %>%
+    fs::file_create()
+  cli::cat_bullet("Created File: ", crayon::green(fs::path(r_int_dir, sourceScriptFile)),
+                  bullet = "tick",
+                  bullet_col = "green")
+
+
+
+  # create output folder
+  outputFolder <- fs::path(path, "output", paste(taskNumber, scriptName, sep = "_"))
+  fs::dir_create(outputFolder)
+  cli::cat_bullet("Created Directory: ", crayon::cyan(outputFolder),
+                  bullet = "tick",
+                  bullet_col = "green")
+
+  #create the source file for script
+  sourceFile <- fs::path("R/internals", sourceScriptFile)
+  source_internals <- glue::glue("source('{sourceFile}')")
+
+  #create outputFolder assignment for script
+  outputFolder_assign <- fs::path("output", paste(taskNumber, scriptName, sep = "_"))
+
+  txt <- c(
+    buildHeader(...),
+    buildDependcies(dependencies),
+    source_internals, "\n",
+    "# Script --------------------\n",
+    glue::glue("outputFolder <- '{outputFolder_assign}' "),
+    "\n"
+  )
+
+  filePath <- fs::path(path, "R", pipelineNm)
+  readr::write_lines(txt, file = filePath)
+  cli::cat_bullet(crayon::green(pipelineNm), " generated at ", crayon::cyan(fs::path(path, "R")),
+                  bullet = "tick", bullet_col = "green")
+  #open file
+  if (openFile) {
+    rstudioapi::navigateToFile(filePath)
+  }
+  invisible(txt)
+
+
 }
